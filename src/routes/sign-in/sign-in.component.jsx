9a85b255d39a7374de044ffcd1afa90adc30ebf0
Miroslav Zografski <mzografski@soft2run.com>
2023-03-09 14:30:52 +0200

    firebase user persist in db

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,4 +1,8 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { 
+	signInWithGooglePopup, 
+	createUserDocumentFromAuth 
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
 	const logGoogleUser = async () => {
 		const {user} = await signInWithGooglePopup();
@@ -8,7 +12,7 @@ const SignIn = () => {
 	return ( 
 		<div className="sign-in-page">
 			<h1>Welcome to Sign In page</h1>
-			<button onClick={logGoogleUser}>Sign in with Google</button>
+			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
 		</div>
 	)
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,7 @@ import {
 	getAuth, 
 	signInWithRedirect, 
 	signInWithPopup, 
-	GoogleAuthProvider 
+	GoogleAuthProvider
 } from 'firebase/auth';
 
 import {
@@ -23,27 +23,24 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
 	prompt: 'select_account'
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
-
 	const userDocRef = doc(db, 'users', userAuth.uid);
-
 	const userSnapshot = await getDoc(userDocRef);
 
 	if (!userSnapshot.exists()) {
 		const { displayName, email } = userAuth;
 		const createdAt = new Date();
-
 		try {
 			await setDoc(userDocRef, {
 				displayName,
