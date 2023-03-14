d44c0c6d5f1fa328549384c72fd95c16d978b502
Miroslav Zografski <mzografski@soft2run.com>
2023-03-14 11:39:46 +0200

    Added cart icon and card dropdown components and related functionality via cart context. Added product-card and extended shop conponent with it. Added products context.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,14 +2,18 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.container';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -24,15 +28,16 @@ const Navigation = () => {
 
           {currentUser ? (
             <span className='nav-link' onClick={signOutUser}>
-              {' '}
-              SIGN OUT{' '}
+              SIGN OUT
             </span>
           ) : (
             <Link className='nav-link' to='/auth'>
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        { isCartOpen && <CartDropdown /> }
       </div>
       <Outlet />
     </Fragment>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,20 @@
-
+import { useContext } from "react";
+import { ProductsContext } from "../../contexts/products.context";
+import "./shop.styles.scss";
+import ProductCard from "../../components/product-card/product-card.component";
 const Shop = () => {
-	return (
-		<div className="shop-page">
-			<h1>Welcome to Shop page</h1>
-		</div>
-	);
+  const { products } = useContext(ProductsContext);
+
+  return (
+    <div className="shop-page">
+      <h1>Welcome to Shop page</h1>
+      <div className="products-container">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
